e84692748e9932ce2aeedc0b6cbeff304ace401a
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-24 18:43:06 -0700

    Add files via upload

---
+++ main.kts
@@ -0,0 +1,20 @@
+
+class Naismith:
+
+fun inFeet(h, mi = 3, ft = 2000){
+    firstFormula = h/mi + h/ft
+}
+
+fun inKilometers(h, km = 5, m = 300){
+    secondFormula = h/km + ((1/2) * h)/ m
+}
+
+class Scarf:
+
+fun calculateDistance(a = 7.92){
+    scarfFormula = x + a * y
+}
+
+fun findingPace(p, a = 7.92, m){
+    pace = p * (1 + a * m)
+}
